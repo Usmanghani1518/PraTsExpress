33b4a829d1b70ed340ae5d692aca668ee5b7a59b
Usmanghani1518 <mrusmanghani8@gmail.com>
2024-08-25 22:50:49 +0500

    feat:Complete all other than login or signUP

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,8 @@ export default {
     port:4000,
     dbUri:"mongodb://localhost:27017/ts-express",
     logLevel:"info",
+    accessTokenPrivateKey:"wertyuioghjkl",
+    refreshTokenPrivateKey:"qwertyuisdfghj",
     smtp:{
         user: "ybyfryry44j5ehnm@ethereal.email",
         pass: "2THfw1tVYSXAGtXfF6",
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import { values } from "lodash";
+import validateResource from "../middleware/validateResource";
+import { CreateUserSchema } from "../schema/user.schema";
+import { createSessionHandler } from "../controller/auth.controller";
+import { CreateSessionSchema } from "../schema/auth.schema";
 
 const router = express.Router();
 
 
+    router.post("/api/v1/session",validateResource(CreateSessionSchema),createSessionHandler)
 
 export default router;
